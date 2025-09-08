8fa1404dc76a310f3f3b0b6a07a396c4dce719d8
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-08 15:07:28 +0100

    Implement StartMenuView component

---
+++ frontend/src/views/startMenu/StartMenuView.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import StartMenuOptions from "$/views/startMenu/components/StartMenuOptions.svelte";
+
+</script>
+
+
+<div class="start-menu">
+   <div class="start-menu-contents">
+       <div class="app-logo">
+           <h1 class="app-name">BOOLD</h1>
+               <span class="version">v0.0.1</span>
+       </div>
+       <StartMenuOptions />
+   </div> 
+</div>
+
+<style>
+    .start-menu{
+        display: grid;
+        place-content: center;
+        background: var(--color-app-black);
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    .start-menu-contents{
+        display: flex;
+        flex-direction: column;
+        place-content: center;
+    }
+    
+    .app-logo{
+        display: flex;
+        align-items: flex-end;
+        margin-block-end: 40px;
+    }
+    
+    .app-name {
+        font-family: var(--font-righteous);
+        color: var(--color-app-white);
+        font-weight: normal;
+        font-size: 10rem;
+        line-height: 0.8;
+    }
+    
+    .version{
+        font-family: var(--font-ubuntu);
+        font-size: 1.125rem;
+        color: var(--color-app-white);
+    }
+</style>

711fe5090f682389d714786132d817c2d7db082f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-08 15:06:20 +0100

    Implement StartMenuOptionsItemDialog component

---
+++ frontend/src/views/startMenu/components/StartMenuOptionsItemDialog.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import type {Snippet} from "svelte";
+
+    type Props = {
+        readonly title: string;
+        readonly isVisible?: boolean;
+        readonly children?: Snippet;
+    }
+    const props = $props();
+</script>
+
+{#if props.isVisible}
+<div class="dialog">
+    <h2>{props.dialogTitle}</h2>
+    {@render props.children?.()}
+</div>
+{/if}
+
+<style>
+    .dialog {
+        background-color: var(--color-app-white);
+    }
+</style>

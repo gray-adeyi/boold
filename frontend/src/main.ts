d7284ce50fa0abf1b44f96b4b56feec51ea5d9da
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-08 15:03:18 +0100

    Add font styles

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,13 @@
 import { mount } from 'svelte'
+// Fonts
+import '@fontsource/material-icons';
+import '@fontsource/righteous';
+import '@fontsource-variable/inconsolata';
+import '@fontsource/ubuntu';
+import '@fontsource/ubuntu-mono';
+import '@fontsource/monaspace-neon';
 import './app.css'
-import App from './App.svelte'
+import App from '$/App.svelte'
 
 const app = mount(App, {
   target: document.getElementById('app')!,

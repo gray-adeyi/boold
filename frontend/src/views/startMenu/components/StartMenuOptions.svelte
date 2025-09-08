70780c854105543ac59f59ea760f3776182bb33a
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-08 15:07:07 +0100

    Implement StartMenuOptions component

---
+++ frontend/src/views/startMenu/components/StartMenuOptions.svelte
@@ -0,0 +1,58 @@
+<script>
+    import StartMenuOptionsItem from "$/views/startMenu/components/StartMenuOptionsItem.svelte";
+    import {Quit} from '$runtime'
+
+    let isNewBoardDialogOpen = $state(false);
+    let isOpenBoardDialogOpen = $state(false);
+    let isSettingsDialogOpen = $state(false);
+
+    const toggleNewBoardDialog = () => { isNewBoardDialogOpen = !isNewBoardDialogOpen }
+    const toggleOpenBoardDialog = () => { isOpenBoardDialogOpen = !isOpenBoardDialogOpen }
+    const toggleSettingsDialog = () => { isSettingsDialogOpen = !isSettingsDialogOpen}
+
+    const options = [
+        {
+            text: "new board",
+            iconName: "add",
+            onclick: toggleNewBoardDialog,
+        },
+        {
+            text: "open board",
+            iconName: "folder",
+            onclick: toggleOpenBoardDialog,
+        },
+        {
+            text: "settings",
+            iconName: "settings",
+            onclick: toggleSettingsDialog,
+        },
+        {
+            text: "quit",
+            iconName: "exit_to_app",
+            onclick: quitApp,
+        }
+    ]
+
+    function quitApp(){
+        Quit()
+    }
+</script>
+
+<nav>
+    <ul>
+        {#each options as option (option.text)}
+            <StartMenuOptionsItem {...option} />
+        {/each}
+    </ul>
+</nav>
+
+<style>
+    nav{
+        display: grid;
+        place-content: center;
+    }
+
+    ul{
+        list-style: none;
+    }
+</style>

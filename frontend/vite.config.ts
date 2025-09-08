60ee0763b1c4ba4c92d1c402747a454aa68b4218
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-08 15:02:40 +0100

    Configure import alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {resolve } from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      '$': resolve(__dirname, 'src'),
+      '$backend': resolve(__dirname, 'wailsjs/go/main/App.js'),
+      '$runtime': resolve(__dirname, 'wailsjs/runtime/runtime.js')
+    }
+  }
 })

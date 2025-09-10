0ef97990c466bc994752253dedcf6fe59934d8e1
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-10 03:36:50 +0100

    Implement StartMenuOptionsItemDialog component

--- frontend/src/views/startMenu/components/StartMenuOptionsItemDialog.svelte
+++ frontend/src/views/startMenu/components/StartMenuOptionsItemDialog.svelte
@@ -2,22 +2,51 @@
     import type {Snippet} from "svelte";
 
     type Props = {
-        readonly title: string;
+        readonly title?: string;
         readonly isVisible?: boolean;
         readonly children?: Snippet;
     }
-    const props = $props();
+    const props: Props = $props();
+
 </script>
 
 {#if props.isVisible}
 <div class="dialog">
-    <h2>{props.dialogTitle}</h2>
+    {#if props.title}<h2 class="title">{props.title}</h2>{/if}
     {@render props.children?.()}
 </div>
 {/if}
 
 <style>
     .dialog {
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+        width: 100%;
+        position: absolute;
+        overflow: visible;
+        z-index: 1;
         background-color: var(--color-app-white);
+        padding-block-end: 50px;
+
+        &::before{
+            --size: 30px;
+            content: "";
+            position: absolute;
+            width: var(--size);
+            height: var(--size);
+            top: calc(-0.5 * var(--size));
+            left: calc(50% + 30px);
+            transform: translateX(-50%) rotate(45deg);
+            z-index: 1;
+            background-color: var(--color-app-white);
+        }
+    }
+
+    .title{
+        font-family: var(--font-ubuntu);
+        font-size: 2rem;
+        font-weight: normal;
+        margin-block: 1.875rem;
     }
 </style>

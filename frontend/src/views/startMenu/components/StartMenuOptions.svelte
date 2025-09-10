a27fa2df146638c050ec5a0f5554df2c9b8303e2
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-10 03:37:38 +0100

    Implement StartMenuOptions component

--- frontend/src/views/startMenu/components/StartMenuOptions.svelte
+++ frontend/src/views/startMenu/components/StartMenuOptions.svelte
@@ -1,30 +1,53 @@
-<script>
+<script lang="ts">
     import StartMenuOptionsItem from "$/views/startMenu/components/StartMenuOptionsItem.svelte";
     import {Quit} from '$runtime'
+    import {navigate} from "$/router";
+    import {createNewBoardDialog} from "./dialogs/CreateNewBoardDialog.svelte";
+
 
     let isNewBoardDialogOpen = $state(false);
     let isOpenBoardDialogOpen = $state(false);
     let isSettingsDialogOpen = $state(false);
 
-    const toggleNewBoardDialog = () => { isNewBoardDialogOpen = !isNewBoardDialogOpen }
-    const toggleOpenBoardDialog = () => { isOpenBoardDialogOpen = !isOpenBoardDialogOpen }
-    const toggleSettingsDialog = () => { isSettingsDialogOpen = !isSettingsDialogOpen}
+    const toggleNewBoardDialog = (event: MouseEvent) => {
+        if(event.ctrlKey){
+            navigate('/workspace')
+        }
+        isOpenBoardDialogOpen = false
+        isSettingsDialogOpen = false
+        isNewBoardDialogOpen = !isNewBoardDialogOpen
+    }
+    const toggleOpenBoardDialog = () => {
+        isNewBoardDialogOpen = false
+        isSettingsDialogOpen = false
+        isOpenBoardDialogOpen = !isOpenBoardDialogOpen }
+    const toggleSettingsDialog = () => {
+        isNewBoardDialogOpen = false
+        isOpenBoardDialogOpen = false
+        isSettingsDialogOpen = !isSettingsDialogOpen}
 
-    const options = [
+    const options = () => [
         {
             text: "new board",
             iconName: "add",
             onclick: toggleNewBoardDialog,
+            dialogTitle: 'Create a new board',
+            isDialogOpen: isNewBoardDialogOpen,
+            dialogComponent: createNewBoardDialog,
         },
         {
             text: "open board",
             iconName: "folder",
             onclick: toggleOpenBoardDialog,
+            dialogTitle: 'Open an existing board',
+            isDialogOpen: isOpenBoardDialogOpen,
         },
         {
             text: "settings",
             iconName: "settings",
             onclick: toggleSettingsDialog,
+            dialogTitle: 'Settings',
+            isDialogOpen: isSettingsDialogOpen,
         },
         {
             text: "quit",
@@ -40,8 +63,8 @@
 
 <nav>
     <ul>
-        {#each options as option (option.text)}
-            <StartMenuOptionsItem {...option} />
+        {#each options() as option (option.text)}
+            <StartMenuOptionsItem {...option}  />
         {/each}
     </ul>
 </nav>
@@ -49,10 +72,11 @@
 <style>
     nav{
         display: grid;
-        place-content: center;
+        place-items: center;
     }
 
     ul{
         list-style: none;
+        width: 100%;
     }
 </style>

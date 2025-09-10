c9a891336ecdcbdbb0b5ba5187f85f98e287ff14
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-10 03:35:27 +0100

    Fix StartMenuView styles

--- frontend/src/views/startMenu/StartMenuView.svelte
+++ frontend/src/views/startMenu/StartMenuView.svelte
@@ -5,19 +5,19 @@
 
 
 <div class="start-menu">
-   <div class="start-menu-contents">
+   <div class="start-menu-contents" >
        <div class="app-logo">
            <h1 class="app-name">BOOLD</h1>
                <span class="version">v0.0.1</span>
        </div>
-       <StartMenuOptions />
+       <StartMenuOptions/>
    </div> 
 </div>
 
 <style>
     .start-menu{
         display: grid;
-        place-content: center;
+        place-items: center;
         background: var(--color-app-black);
         width: 100%;
         height: 100vh;
@@ -28,10 +28,13 @@
         display: flex;
         flex-direction: column;
         place-content: center;
+        width: 100%;
+        transition: transform 0.5s ease-in-out;
     }
     
     .app-logo{
         display: flex;
+        justify-content: center;
         align-items: flex-end;
         margin-block-end: 40px;
     }

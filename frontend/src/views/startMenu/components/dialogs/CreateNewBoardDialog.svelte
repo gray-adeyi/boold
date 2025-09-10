05530362977bf16301b4d5be6bd77d8075d29d24
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-10 03:38:02 +0100

    Implement CreateNewBoardDialog component

---
+++ frontend/src/views/startMenu/components/dialogs/CreateNewBoardDialog.svelte
@@ -0,0 +1,103 @@
+<script lang="ts" module>
+    export {createNewBoardDialog}
+</script>
+
+<script>
+    import DefaultButton from "$/lib/components/DefaultButton.svelte";
+    import MacCmdKeyIcon from "$/assets/icons/mac-command.svg?component";
+    import MouseLeftButtonIcon from "$/assets/icons/mouse-left-button.svg?component";
+
+</script>
+
+{#snippet createNewBoardDialog() }
+    <div class="input-and-tip-container">
+        <input placeholder="Board Name"/>
+        <p class="tip">This board will be saved as new-board.board</p>
+    </div>
+
+    <p class="hint">Hint:
+        <MacCmdKeyIcon width="24" height="24"  /> +
+        <MouseLeftButtonIcon width="24" height="24" />
+        (Cmd or Ctrl + Click) Starts a new board in sandbox mode
+    </p>
+    <div class="button-container">
+        <DefaultButton text="Cancel"/>
+        <DefaultButton text="Create"/>
+    </div>
+{/snippet}
+
+<style>
+    .input-and-tip-container {
+        display: flex;
+        flex-direction: column;
+        place-content: center;
+        margin-block-end: 1rem;
+
+        &:focus-within .tip {
+            opacity: 1;
+        }
+    }
+
+    input {
+        border: none;
+        padding: .625rem;
+        background: transparent;
+        border-bottom: 2px solid var(--color-app-gray);
+        outline: none;
+        font-family: var(--font-ubuntu);
+        font-size: 1.25rem;
+        transition: border-bottom-color .3s;
+
+        /*&::selection{*/
+        /*    background: var(--color-app-black);*/
+        /*    color: var(--color-app-white);*/
+        /*}*/
+
+        &::placeholder {
+            color: var(--color-app-gray);
+        }
+
+        &:focus {
+            border-bottom: 2px solid var(--color-app-black);
+        }
+    }
+
+    .tip {
+        padding: .625rem;
+        background: var(--color-app-black-alt);
+        border-radius: 5px;
+        position: relative;
+        margin-block-start: 12px;
+        font-size: .9375rem;
+        opacity: 0;
+        transition: opacity .3s;
+
+        &::before {
+            --size: 15px;
+            content: '';
+            position: absolute;
+            width: var(--size);
+            height: var(--size);
+            top: -7px;
+            left: 50%;
+            background-color: var(--color-app-black-alt);
+            transform: rotate(45deg);
+        }
+    }
+
+    .icon{
+        fill: var(--color-app-black);
+    }
+
+    .hint {
+        color: var(--color-app-gray);
+        margin-block-end: 1rem;
+        font-family: var(--font-ubuntu);
+    }
+
+    .button-container {
+        display: flex;
+        place-items: center;
+        gap: 20px;
+    }
+</style>

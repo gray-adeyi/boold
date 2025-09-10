df240eec34cdf2f272b071ebfcda814ba28c13a3
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-10 03:33:46 +0100

    Configure vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,21 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import {resolve } from 'node:path'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {resolve} from 'node:path'
 
+import svelteSVG from "@hazycora/vite-plugin-svelte-svg";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      '$': resolve(__dirname, 'src'),
-      '$backend': resolve(__dirname, 'wailsjs/go/main/App.js'),
-      '$runtime': resolve(__dirname, 'wailsjs/runtime/runtime.js')
+    plugins: [svelte(),
+        svelteSVG({
+            svgoConfig: {}, // See https://github.com/svg/svgo#configuration
+            requireSuffix: true, // Set false to accept '.svg' without the '?component'
+        }),
+    ],
+    resolve: {
+        alias: {
+            '$': resolve(__dirname, 'src'),
+            '$backend': resolve(__dirname, 'wailsjs/go/main/App.js'),
+            '$runtime': resolve(__dirname, 'wailsjs/runtime/runtime.js')
+        }
     }
-  }
 })

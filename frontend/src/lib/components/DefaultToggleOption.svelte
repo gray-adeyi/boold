eca89340991c56b25453d8c8d464ee352d267c2f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-21 22:58:40 +0100

    Implement DefaultToggleOption component

---
+++ frontend/src/lib/components/DefaultToggleOption.svelte
@@ -0,0 +1,15 @@
+<script>
+    import DefaultToggleSwitch from "$/lib/components/DefaultToggleSwitch.svelte";
+</script>
+
+<div class="option">
+    <span>Scroll animation</span>
+    <DefaultToggleSwitch />
+</div>
+
+<style>
+    .option{
+        display: flex;
+        justify-items: space-between;
+    }
+</style>

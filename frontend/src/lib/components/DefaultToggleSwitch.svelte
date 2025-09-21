2828ba35098498d64c278c52263a22f3eeaa0722
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-09-21 22:58:52 +0100

    Implement DefaultToggleSwitch component

---
+++ frontend/src/lib/components/DefaultToggleSwitch.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+    // type Props = {
+    //     checked: boolean;
+    //     onToggle: (checked: boolean) => void;
+    // }
+    // const {checked, onToggle}: Props = $props();
+    // const toggle = () => {}
+    let checked = $state(false);
+    const ariaChecked = $derived(checked ? 'true' : 'false');
+
+    const toggle = () => {
+        checked = !checked
+    }
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Enter' || event.key === ' ') {
+            toggle();
+        }
+    }
+</script>
+
+<div class={["switch",{'switch--checked': checked}]} role="switch" onclick={toggle} onkeydown={handleKeyDown}
+     aria-checked={ariaChecked} tabindex="0">
+    <span class="slider"></span>
+</div>
+
+<style>
+    .switch {
+        display: flex;
+        width: 36px;
+        height: 22px;
+        background: var(--color-app-gray);
+        padding: 4px;
+        cursor: pointer;
+        transition: background .2s;
+    }
+
+    .slider {
+        --size: 14px;
+        width: var(--size);
+        height: var(--size);
+        background: var(--color-app-white);
+        transition: transform .2s;
+    }
+
+    .switch--checked {
+        background: var(--color-app-black);
+
+        .slider {
+            transform: translateX(var(--size));
+        }
+    }
+
+</style>

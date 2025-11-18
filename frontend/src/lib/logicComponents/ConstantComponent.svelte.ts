682bfe5de30c974c9092b9a34b02e0a431b16819
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-11-18 13:33:17 +0100

    Implement ConstantComponent

---
+++ frontend/src/lib/logicComponents/ConstantComponent.svelte.ts
@@ -0,0 +1,15 @@
+import type { Coord } from "$/types";
+import PrimitiveComponent from "./PrimitiveComponent.svelte";
+
+export default class ConstantComponent extends PrimitiveComponent {
+  constructor(name: string | null, pos: Coord, value: number = 0) {
+    super(name, pos, 2, 1, { type: "value" });
+    this.addOutputPin({ side: 1, pinIndex: 0 }, "OUT");
+    this.value = value;
+  }
+
+  execute(): void {
+    if (this.value === null) return;
+    this.outputPins[0].value = this.value;
+  }
+}

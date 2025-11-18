096a607ea9b9e8f4a671b2e031907cb8c949b83f
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-11-18 14:24:32 +0100

    Implement DebugComponent

---
+++ frontend/src/lib/logicComponents/DebugComponent.svelte.ts
@@ -0,0 +1,17 @@
+import type { Coord } from "$/types";
+import PrimitiveComponent from "./PrimitiveComponent.svelte";
+
+export default class DebugComponent extends PrimitiveComponent {
+  constructor(name: string | null, pos: Coord) {
+    super(name, pos, 2, 1, { type: "icon", text: "report_problem" });
+    this.addInputPin({ side: 3, pinIndex: 0 }, "IN");
+    this.value = 0;
+  }
+
+  execute(): void {
+    if (this.value === null) return;
+    this.inputPins[0].value = this.value;
+    // notifications.push(`${this.name}: ${this.value}`)
+    // boolrConsole.log(`${this.name}: ${this.value}`)
+  }
+}

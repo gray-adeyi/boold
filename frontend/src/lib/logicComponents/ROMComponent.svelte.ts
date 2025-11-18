6ca06b4dc5f8493e6bc39f56b2a26c07b9218c09
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-11-18 14:25:25 +0100

    Implement ROMComponent

---
+++ frontend/src/lib/logicComponents/ROMComponent.svelte.ts
@@ -0,0 +1,23 @@
+import type { Coord } from "$/types";
+import PrimitiveComponent from "./PrimitiveComponent.svelte";
+
+
+
+export default class ROMComponent extends PrimitiveComponent {
+  constructor(name: string | null, pos: Coord, data = []){
+    super(name, pos, 3, 8, {type: "char", text: "ROM"})
+  }
+  
+  execute(): void {
+    let addr = 0
+    for (let i = 0; i < this.inputPins.length; i++){
+    addr |= (this.inputPins[i].value > 0) << i
+    }
+    const rom = this.properties.rom 
+    if(!rom) return
+    const content = rom[addr]
+    for (let i = 0; i < this.outputPins.length; i++){
+      this.outputPins[i].value = (content & (1 << i)) > 0 ? 1 : 0
+    }
+  }
+}

3246c7ed02d5c419b5347032d8fb126a13b9d61d
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-11-18 14:23:53 +0100

    Implement CounterComponent

---
+++ frontend/src/lib/logicComponents/CounterComponent.svelte.ts
@@ -0,0 +1,14 @@
+import type { Coord } from "$/types";
+import PrimitiveComponent from "./PrimitiveComponent.svelte";
+
+export default class CounterComponent extends PrimitiveComponent {
+  constructor(name: string | null, pos: Coord) {
+    super(name, pos, 2, 1, { type: "value" });
+    this.addInputPin({ side: 3, pinIndex: 0 }, "IN");
+    this.value = 0;
+  }
+
+  execute(): void {
+    if (this.inputPins[0].value === 1 && this.value === 0) this.value++;
+  }
+}

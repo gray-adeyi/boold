1df884a1b957801e0619d5311b8866e87be0a188
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-11-18 13:18:44 +0100

    Implement ORGateComponent

---
+++ frontend/src/lib/logicComponents/ORGateComponent.svelte.ts
@@ -0,0 +1,16 @@
+import type { Coord } from "$/types";
+import PrimitiveComponent from "./PrimitiveComponent.svelte";
+
+export default class ORGateComponent extends PrimitiveComponent {
+  constructor(name: string | null, pos: Coord) {
+    super(name, pos, 2, 2, { type: "char", text: "|" });
+    this.addInputPin({ side: 3, pinIndex: 0 }, "A");
+    this.addInputPin({ side: 3, pinIndex: 1 }, "B");
+    this.addOutputPin({ side: 1, pinIndex: 0 }, "OUT");
+  }
+
+  execute(): void {
+    this.outputPins[0].value =
+      this.inputPins[0].value | this.inputPins[1].value;
+  }
+}

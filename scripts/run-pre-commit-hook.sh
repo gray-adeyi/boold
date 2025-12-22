7e75bc4f1d724652f16e7abf32ced103b01419ed
Gbenga Adeyi <adeyigbenga005@gmail.com>
2025-12-22 22:56:07 +0100

    Add precommit hook script

--- scripts/run-pre-commit-hook.sh
+++ scripts/run-pre-commit-hook.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+GO_PATH="$HOME/go/go1.24.11.linux-amd64/bin"
+BUN_PATH="$HOME/.bun/bin"
+TASK="/usr/bin/go-task"
+
+export PATH=$BUN_PATH:$GO_PATH:$PATH
+
 echo "running precommit hooks..."
 
-make fmt-all
+
+$TASK fmt-all
 
 echo "Done!"

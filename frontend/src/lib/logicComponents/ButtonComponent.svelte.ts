8236b9e753b7ddff01a9332973fcd6598ddecd88
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-01-12 19:43:03 +0100

    Implement button component

--- frontend/src/lib/logicComponents/ButtonComponent.svelte.ts
+++ frontend/src/lib/logicComponents/ButtonComponent.svelte.ts
@@ -1,27 +1,24 @@
 import PrimitiveComponent from "$/lib/logicComponents/PrimitiveComponent.svelte";
 import type { BoardStoreState } from "$/stores/boardStore.svelte";
 import type { Coord } from "$/types";
+import type ClickableComponent, {
+  ComponentClickEventType,
+} from "$/lib/logicComponents/ClickableComponent";
 
-export default class ButtonComponent extends PrimitiveComponent {
-	constructor(
-		name: string | null,
-		pos: Coord,
-		boardStoreState: BoardStoreState,
-	) {
-		super(
-			name,
-			pos,
-			2,
-			1,
-			{ type: "icon", text: "radio_button_checked" },
-			boardStoreState,
-		);
-		this.addOutputPin({ side: 1, sideIndex: 0 }, "OUT");
-		this.value = 0;
-	}
+export default class ButtonComponent extends PrimitiveComponent implements ClickableComponent {
+  constructor(name: string | null, pos: Coord, boardStoreState: BoardStoreState) {
+    super(name, pos, 2, 1, { type: "icon", text: "radio_button_checked" }, boardStoreState);
+    this.addOutputPin({ side: 1, sideIndex: 0 }, "OUT");
+    this.value = 0;
+  }
 
-	execute(): void {
-		if (this.value === null) return;
-		this.outputPins[0].value = this.value;
-	}
+  handleClick(eventType: ComponentClickEventType): void {
+    this.value = eventType === "mouseup" ? 0 : 1;
+    this.update();
+  }
+
+  execute(): void {
+    if (this.value === null) return;
+    this.outputPins[0].value = this.value;
+  }
 }

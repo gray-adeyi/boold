7ff2c4dd07adbff485353ff80c9b2eb609dd7f8e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2026-01-12 19:41:03 +0100

    Refactor constant component

--- frontend/src/lib/logicComponents/ConstantComponent.svelte.ts
+++ frontend/src/lib/logicComponents/ConstantComponent.svelte.ts
@@ -3,20 +3,19 @@ import PrimitiveComponent from "$/lib/logicComponents/PrimitiveComponent.svelte"
 import type { BoardStoreState } from "$/stores/boardStore.svelte";
 
 export default class ConstantComponent extends PrimitiveComponent {
-	constructor(
-		name: string | null,
-		pos: Coord,
-		value: number = 0,
+  constructor(
+    name: string | null,
+    pos: Coord,
+    boardStoreState: BoardStoreState,
+    value: number = 0,
+  ) {
+    super(name, pos, 2, 1, { type: "value" }, boardStoreState);
+    this.addOutputPin({ side: 1, sideIndex: 0 }, "OUT");
+    this.value = value;
+  }
 
-		boardStoreState: BoardStoreState,
-	) {
-		super(name, pos, 2, 1, { type: "value" }, boardStoreState);
-		this.addOutputPin({ side: 1, sideIndex: 0 }, "OUT");
-		this.value = value;
-	}
-
-	execute(): void {
-		if (this.value === null) return;
-		this.outputPins[0].value = this.value;
-	}
+  execute(): void {
+    if (this.value === null) return;
+    this.outputPins[0].value = this.value;
+  }
 }
